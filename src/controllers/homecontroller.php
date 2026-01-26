0cdcbf051b5735265618436368eacee486b788df
DaRealNiekie <niekdehaan1003@gmail.com>
2026-01-26 10:52:52 +0100

    huiswerk opdracht 6 interface

--- src/controllers/homecontroller.php
+++ src/controllers/homecontroller.php
@@ -1,22 +1,26 @@
 <?php
- 
+
+
 declare(strict_types=1);
- 
+
 namespace App\Controllers;
- 
-use GuzzleHttp\Psr7\Response;
+
+
 use GuzzleHttp\Psr7\Utils;
- 
+use Nyholm\Psr7\Response as NyholmResponse;
+use Psr\Http\Message\ResponseInterface;
+
 class HomeController
 {
-    public function index(): Response
+    public function index(): ResponseInterface
     {
         $stream = Utils::streamFor("Dit is de homepagina voor de zoutstrooimanagement!");
- 
-        $response = new Response();
- 
+
+        $response = new NyholmResponse;
+
         $response = $response->withBody($stream);
- 
+
         return $response;
+
     }
 }
--- src/controllers/productcontroller.php
+++ src/controllers/productcontroller.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use GuzzleHttp\Psr7\Response;
-use GuzzleHttp\Psr7\ServerRequest;
 use GuzzleHttp\Psr7\Utils;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 class ProductController
 {
-    public function index(): Response
+    public function index(): ResponseInterface
     {
         $stream = Utils::streamFor("list of products");
 
@@ -21,7 +22,7 @@ class ProductController
         return $response;
     }
 
-    public function show(ServerRequest $request, array $args): Response
+    public function show(ServerRequestInterface $request, array $args): ResponseInterface
     {
         $id = $args["id"];
 
